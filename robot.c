f03300e3f3595662e480fb6985b52d41ee3a60e9
sofianecharifi <sofiane.charifialaoui@gmail.com>
2025-10-19 19:39:15 +0200

    Modify robot function to use grid for valid moves

--- robot.c
+++ robot.c
@@ -2,8 +2,16 @@
 #include <stdio.h>
 #include <time.h>
 
-int robot()
+int robot(char grid[9]) 
 {
-    srand(time(NULL));
-    int nombre_random = rand() % 9;
+    int move;
+    srand(time(NULL));              // initialise le générateur aléatoire
+    move = rand() % 9;              // premier tirage (0 à 8)
+
+    while (grid[move] != ' ') 
+    {
+        move = rand() % 9;
+    }
+
+    return move + 1; 
 }
